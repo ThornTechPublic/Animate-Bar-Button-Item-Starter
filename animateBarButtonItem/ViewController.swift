1794f513d56d5a54af63b1a3510a73291faada0a
Robert Chen <robert.chen@thorntechnologies.com>
2015-08-16 18:51:13 -0400

    rolling back to the start state

--- animateBarButtonItem/ViewController.swift
+++ animateBarButtonItem/ViewController.swift
@@ -10,60 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var rightBarButton: UIBarButtonItem! {
-        didSet {
-            // using image from asset catalog
-            let icon = UIImage(named: "star")
-            // need the icon size for the button
-            let iconSize = CGRect(origin: CGPointZero, size: icon!.size)
-            // create a button using the icon size
-            let iconButton = UIButton(frame: iconSize)
-            // set the button image
-            iconButton.setBackgroundImage(icon, forState: .Normal)
-            // custom view breaks the IBAction, so set the target manually
-            iconButton.addTarget(self, action: "tappedRightButton", forControlEvents: .TouchUpInside)
-            // put the button in the right bar button item
-            rightBarButton.customView = iconButton
-            // This is to support the initial animation.
-            // First stage the button to be microscopic
-            rightBarButton.customView!.transform = CGAffineTransformMakeScale(0, 0)
-            
-            // animate the button to normal size
-            UIView.animateWithDuration(1.0,
-                delay: 0.5,
-                // between 0.0 and 1.0, this is the brakes applied to the bounciness
-                usingSpringWithDamping: 0.5,
-                // approximate pixels per second you want to explode the button
-                initialSpringVelocity: 10,
-                options: .CurveLinear,
-                animations: {
-                    // restore the button to original size.
-                    // it may briefly grow past normal size,
-                    // depending on how high you set the spring velocity.
-                    self.rightBarButton.customView!.transform = CGAffineTransformIdentity
-                },
-                completion: nil
-            )
-        }
+    @IBAction func tappedStar(sender: AnyObject) {
+        println("tapped star")
     }
     
     override func viewDidLoad() {
-        // thanks to didSet on IBOutlet, this viewDidLoad is pretty clean.
-    }
-    
-    func tappedRightButton(){
-        println("tapped right button")
-        spinTheRightBarButton()
-    }
 
-    // spin the star when tapped
-    func spinTheRightBarButton(){
-        // wind the clock back
-        rightBarButton.customView!.transform = CGAffineTransformMakeRotation(CGFloat(M_PI * 6/5))
-        // animate back to original position
-        UIView.animateWithDuration(1.0, animations: {
-            self.rightBarButton.customView!.transform = CGAffineTransformIdentity
-        })
     }
     
 }
